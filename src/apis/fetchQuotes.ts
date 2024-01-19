891b26ba107f987aaecffbeb9b35f3b8aaf922cc
simuratli <simuratli@gmail.com>
2024-01-19 21:20:16 +0400

    quote

--- src/apis/fetchQuotes.ts
+++ src/apis/fetchQuotes.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchQuotes = async () => {
   const category = 'happiness';
   const headers = {
-    'X-Api-Key': process.env.QUOTE_API_KEY,
+    'X-Api-Key':"HwTkcWTsycCTdwtEnjFFSQ==yI6Z3lFo344ZjywA",
   };
 
   const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
