2606e3992706bd0412ae529dd39071543286d263
simuratli <simuratli@gmail.com>
2024-01-19 21:07:45 +0400

    quote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,35 @@ import { Categories, Books } from '@/containers';
 import { BannerCard, Quote } from '@/components';
 import { HomePagePropTypes } from '@/types/pageProp.types';
 // import { fetchQuotes } from '@/apis/fetchQuotes';
-import axios from 'axios';
+// import axios from 'axios';
 
 
-export const getServerSideProps = async () => {
-  // const quote = await fetchQuotes();
+// export const getStaticProps = async () => {
+//   // const quote = await fetchQuotes();
+//
+//   const category = 'happiness';
+//   const headers = {
+//     'X-Api-Key': process.env.QUOTE_API_KEY,
+//   };
+//
+//   const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+//     headers: headers,
+//   });
+//   const quote = response.data[0];
+//
+//
+//   return {
+//     props: { quote },
+//   };
+// };
 
-  const category = 'happiness';
-  const headers = {
-    'X-Api-Key': process.env.QUOTE_API_KEY,
-  };
-
-  const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-    headers: headers,
-  });
-  const quote = response.data[0];
-
-
-  return {
-    props: { quote },
-  };
-};
+export const getServerSideProps = (async () => {
+  // Fetch data from external API
+  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const repo: Repo = await res.json()
+  // Pass data to the page via props
+  return { props: { repo } }
+}) satisfies GetServerSideProps<{ repo: Repo }>
 
 
 function Home({quote}:HomePagePropTypes) {
