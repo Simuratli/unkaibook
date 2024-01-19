6e05c067a7a1de11420f8037d3d5d578e8599053
simuratli <simuratli@gmail.com>
2024-01-19 21:09:31 +0400

    quote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ import { HomePagePropTypes } from '@/types/pageProp.types';
 export const getServerSideProps = (async () => {
   // Fetch data from external API
   const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const repo: Repo = await res.json()
+  const repo:any = await res.json()
   // Pass data to the page via props
   return { props: { repo } }
 })
