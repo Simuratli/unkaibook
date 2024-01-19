3f3adc534b87e3bd9d0a0cc39eb28d6e11183a9d
simuratli <simuratli@gmail.com>
2024-01-19 21:04:05 +0400

    quote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,30 @@ import { Categories, Books } from '@/containers';
 import { BannerCard, Quote } from '@/components';
 import { HomePagePropTypes } from '@/types/pageProp.types';
 // import { fetchQuotes } from '@/apis/fetchQuotes';
-// import axios from 'axios';
-function Home({quote}:HomePagePropTypes) {
+import axios from 'axios';
 
+
+export const getStaticProps = async () => {
+  // const quote = await fetchQuotes();
+
+  const category = 'happiness';
+  const headers = {
+    'X-Api-Key': process.env.QUOTE_API_KEY,
+  };
+
+  const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+    headers: headers,
+  });
+  const quote = response.data[0];
+
+
+  return {
+    props: { quote },
+  };
+};
+
+
+function Home({quote}:HomePagePropTypes) {
   return (
     <div className='main'>
       {quote && <Quote quote={quote.quote} author={quote.author} />}
@@ -17,23 +38,6 @@ function Home({quote}:HomePagePropTypes) {
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const quote = await fetchQuotes();
-//
-//   const category = 'happiness';
-//   const headers = {
-//     'X-Api-Key': process.env.QUOTE_API_KEY,
-//   };
-//
-//   const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-//     headers: headers,
-//   });
-//   const quote = response.data[0];
-//
-//
-//   return {
-//     props: { quote },
-//   };
-// };
+
 
 export default Home;
