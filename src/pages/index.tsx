5d47ef4bd84623d2eed1314a64feeb608913e197
simuratli <simuratli@gmail.com>
2024-01-19 20:57:09 +0400

    quote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Categories, Books } from '@/containers';
 import { BannerCard, Quote } from '@/components';
 import { HomePagePropTypes } from '@/types/pageProp.types';
-import { fetchQuotes } from '@/apis/fetchQuotes';
+// import { fetchQuotes } from '@/apis/fetchQuotes';
+import axios from 'axios';
 function Home({ quote }: HomePagePropTypes) {
 
   return (
@@ -17,7 +18,19 @@ function Home({ quote }: HomePagePropTypes) {
 }
 
 export const getStaticProps = async () => {
-  const quote = await fetchQuotes();
+  // const quote = await fetchQuotes();
+
+  const category = 'happiness';
+  const headers = {
+    'X-Api-Key': process.env.QUOTE_API_KEY,
+  };
+
+  const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+    headers: headers,
+  });
+  const quote = response.data[0];
+
+
   return {
     props: { quote },
   };
