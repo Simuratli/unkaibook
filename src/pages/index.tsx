9310e2ed7bb9343906dc36c0f55711a9f9c34f69
simuratli <simuratli@gmail.com>
2024-01-19 21:22:21 +0400

    quote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,22 @@
 import React from 'react';
 import { Categories, Books } from '@/containers';
-import { BannerCard } from '@/components';
-import { QuoteType } from '@/types/pageProp.types';
+import { BannerCard, Quote } from '@/components';
+import { HomePagePropTypes } from '@/types/pageProp.types';
 import { fetchQuotes } from '@/apis/fetchQuotes';
-// import axios from 'axios';
 
 
-// export const getStaticProps = async () => {
-//   // const quote = await fetchQuotes();
-//
-//   const category = 'happiness';
-//   const headers = {
-//     'X-Api-Key': process.env.QUOTE_API_KEY,
-//   };
-//
-//   const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-//     headers: headers,
-//   });
-//   const quote = response.data[0];
-//
-//
-//   return {
-//     props: { quote },
-//   };
-// };
-
-export const getServerSideProps = (async () => {
-  // Fetch data from external API
-  const quote:QuoteType = await fetchQuotes();
-  console.log(quote)
-  // const repo:any = await res.json()
-  // Pass data to the page via props
-  return { props: { res:'salam' } }
-})
+export const getStaticProps = async () => {
+  const quote = await fetchQuotes()
+  return {
+    props: { quote },
+  };
+};
 
 
-function Home({res}: { res:string }) {
-  console.log(res)
+function Home({quote}:HomePagePropTypes) {
   return (
     <div className='main'>
-      {/*{quote && <Quote quote={quote.quote} author={quote.author} />}*/}
+      {quote && <Quote quote={quote.quote} author={quote.author} />}
       <Categories />
       <Books />
       <BannerCard />
